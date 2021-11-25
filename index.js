278fa346b302e922c0742da3e5f8742ff8ffcbeb
Amos Tobi <tobideveloper@gmail.com>
2021-11-25 20:12:54 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/* eslint-disable array-callback-return */
+const { badRequest } = require('@hapi/boom');
+const Ajv = require('ajv');
+
+const validate = (schema) => function validateRequest(req, res, next) {
+  const toValidate = {};
+  const errors = [];
+
+  if (!schema) {
+    return next();
+  }
+
+  ['params', 'body', 'query'].forEach((key) => {
+    if (schema[key]) {
+      toValidate[key] = req[key];
+    }
+  });
+
+  const ajv = new Ajv();
+
+  Object.keys(toValidate).map((key) => {
+    const ajvValidate = ajv.compile(schema[key]);
+
+    const valid = ajvValidate(toValidate[key]);
+
+    if (!valid) {
+      errors.push(ajvValidate.errors[0]);
+    }
+  });
+
+  if (errors.length) {
+    return next(badRequest('Validation Error', errors));
+  }
+
+  return next();
+};
+
+module.exports = validate;
